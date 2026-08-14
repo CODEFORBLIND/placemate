323dfdff7ef284ab2d6270860cea232fdef2d6e1
nikhil-thorat <nikhilthoratofficial@gmail.com>
2026-08-14 15:53:25 +0530

    misc: testing theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import Logo from "@/components/logo";
 
 export default function Home() {
-  return (
-    <div>Hello world!</div>
-  );
+    return (
+        <div className="flex flex-col gap-8 items-center justify-center min-h-screen space-x-4">
+            <Logo />
+            <p className="text-muted-foreground">Multi Agent Placement Cell Activity Automation Platform</p>
+        </div>
+    );
 }
